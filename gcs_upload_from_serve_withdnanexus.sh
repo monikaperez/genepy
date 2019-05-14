b09d1db768ddf9c10cf63c2a0de9b47f7b1d8d9f
Jérémie Kalfon <jkobject@gmail.com>
2019-05-14 14:12:50 -0400

    file to download data from server to bucket using dnanexus

---
+++ gcs_upload_from_serve_withdnanexus.sh
@@ -0,0 +1,8 @@
+#!bin/bash 
+# listing the files
+dx ls "immediate/bam/*.WholeGenome*" >list.txt
+# getting the urls
+for i in $(cat list.txt):
+do dx make_download_url --duration "1w" "immediate/bam/$i" >> urls.txt; done
+# for 8 of them at a time, downloading them directly to the bucket in parrallel 
+nohup cat urls.txt | xargs -P 8 -n 1 wget -q | gsutil cp - gs://fc-secure-fd4c24cf-6bfd-410a-9bca-e02642da12f8 > nohup.out 2>&1 &
