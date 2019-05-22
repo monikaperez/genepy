203417a6e490a38f422c12c9684c06119c7830a6
jkobject <jkobject@gmail.com>
2019-05-22 19:33:06 +0000

    better upload server

--- gcs_upload_from_serve_withdnanexus.sh
+++ gcs_upload_from_serve_withdnanexus.sh
@@ -5,4 +5,4 @@ dx ls "immediate/bam/*.WholeGenome*" >list.txt
 for i in $(cat list.txt):
 do dx make_download_url --duration "1w" "immediate/bam/$i" >> urls.txt; done
 # for 8 of them at a time, downloading them directly to the bucket in parrallel 
-nohup cat urls.txt | xargs -P 8 -n 1 wget -q | gsutil cp - gs://fc-secure-fd4c24cf-6bfd-410a-9bca-e02642da12f8 > nohup.out 2>&1 &
+nohup cat urls.txt | xargs -P 8 -n 1 wget -qO- | gsutil cp - 'gs://fc-secure-fd4c24cf-6bfd-410a-9bca-e02642da12f8/immediate/' > nohup.out 2>&1 &
