3cf8e5d8f7b0a31c92d5d316ee3d746a9cf5b991
Jérémie Kalfon <jkobject@gmail.com>
2020-01-15 09:50:41 -0500

    uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("README", 'r') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
@@ -9,20 +9,22 @@ setup(
     description='A useful module for any CompBio',
     author='Jeremie Kalfon',
     author_email='jkobject@gmail.com',
-    packages=['JKBio', 'cell-line-mapping', 'pyDESeq2'],  # same as name
+    url="https://github.com/jkobject/JKBio",
+    packages=['cell_line_mapping-master/python'],  # same as name
+    python_requires='>=3.5',
     install_requires=[
             'pysam',
             'numpy',
-	        'pandas',
-	        'venn',
-	        'sklearn',
-	        'seaborn',
-	        'scikit-learn',
-	        'rpy2',
-	        'rpy2-bioconductor-extensions',
-	        'pysam',
-	        'jupyter',
-	        'gseapy',
-	        'bokeh',
-	        'igv'],  # external packages as dependencies
+        'pandas',
+        'venn',
+        'sklearn',
+        'seaborn',
+        'scikit-learn',
+        'rpy2',
+        'rpy2-bioconductor-extensions',
+        'pysam',
+        'jupyter',
+        'gseapy',
+        'bokeh',
+        'igv'],  # external packages as dependencies
 )
