efd39be10467c33758a07d2cbf0915818bb1f9b5
jkobject <jkobject@gmail.com>
2021-02-17 18:05:28 +0000

    debugging

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 import sys
+import os
 if sys.version_info.major < 3 or sys.version_info.minor < 2:
     raise ValueError("genepy is only compatible with Python 3.3 and below")
 if sys.version_info.minor < 5:
     import warnings
     warnings.warn("genepy may not function properly on Python < 3.5")
+
+os.system('git submodule init && git submodule sync')
 
 with open("README.md", 'r') as f:
     long_description = f.read()
@@ -13,15 +16,15 @@ setup(
     name='Broad-genepy',
     version='1.0',
     description='A useful module for any CompBio',
-    long_description = long_description,
+    long_description=long_description,
     author='Jeremie Kalfon',
     author_email='jkobject@gmail.com',
     url="https://github.com/BroadInstitute/genepy",
-    packages=['genepy/cell_line_mapping-master/python',
-              'genepy/epigenetics/rose'],  # same as name
+    packages=['genepy/cell_line_mapping-master/python/cell_line_mapper',
+              'genepy/epigenetics', 'genepy/mutations', 'genepy/google', 'genepy/sequencing/',
+              'genepy/terra', 'genepy/rna', 'genepy/utils'],
     package_data={'genepy': ['data/*']},
     python_requires='>=3.5',
-    scripts=['genepy'],
     install_requires=[
         'rpy2-bioconductor-extensions',
         'gseapy',
