d45b1b374c5e079d4f5ba9b671b481061736efd0
jkobject <jkobject@gmail.com>
2021-08-27 19:22:24 +0000

    cleaning up indents

--- genepy/google/google_sheet.py
+++ genepy/google/google_sheet.py
@@ -6,10 +6,10 @@ scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/au
 
 
 def dfToSheet(df, sheetid, secret='~/.credentials.json'):
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(secret, scope)
-    client = gspread.authorize(credentials)
-    spreadsheet = client.open(sheetid)
-    df.to_csv('/tmp/sheet.csv')
-    with open("/tmp/sheet.csv", 'r') as file_obj:
-        content = file_obj.read()
-        client.import_csv(spreadsheet.id, data=content)
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(secret, scope)
+  client = gspread.authorize(credentials)
+  spreadsheet = client.open(sheetid)
+  df.to_csv('/tmp/sheet.csv')
+  with open("/tmp/sheet.csv", 'r') as file_obj:
+    content = file_obj.read()
+    client.import_csv(spreadsheet.id, data=content)
