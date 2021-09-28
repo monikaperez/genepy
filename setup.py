950f94ec4e4303b4c61f13d9e8c1148593d40d7b
jkobject <jkobject@gmail.com>
2021-09-28 16:02:57 +0000

    updating setup

--- setup.py
+++ setup.py
@@ -12,9 +12,34 @@ os.system('git submodule init && git submodule sync')
 with open("README.md", 'r') as f:
     long_description = f.read()
 
+
+def read(*paths, **kwargs):
+  """Read the contents of a text file safely.
+  >>> read("genepy", "VERSION")
+  '0.1.0'
+  >>> read("README.md")
+  ...
+  """
+
+  content = ""
+  with io.open(
+    os.path.join(os.path.dirname(__file__), *paths),
+    encoding=kwargs.get("encoding", "utf8"),
+  ) as open_file:
+    content = open_file.read().strip()
+  return content
+
+
+def read_requirements(path):
+  return [
+    line.strip()
+    for line in read(path).split("\n")
+    if not line.startswith(('"', "#", "-", "git+"))
+  ]
+
 setup(
     name='Broad-genepy',
-    version='1.0',
+    version=read("genepy", "VERSION"),
     description='A useful module for any CompBio',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,38 +51,7 @@ setup(
               'genepy/terra', 'genepy/rna', 'genepy/utils'],
     package_data={'genepy': ['data/*']},
     python_requires='>=3.5',
-    install_requires=[
-        'rpy2-bioconductor-extensions',
-        'gseapy',
-        'macs2',
-        'deeptools',
-        ## from requirements.txt
-        "bokeh",
-        "colorcet",
-        "firecloud_dalmatian",
-        "gseapy",
-        "biomart",
-        "gsheets",
-        "gspread",
-        "matplotlib",
-        "numpy",
-        "oauth2client",
-        "pandas",
-        "Pillow",
-        "pybedtools",
-        "pyBigWig",
-        "pysam",
-        "pytest",
-        "requests",
-        "rpy2",
-        "itermplot",
-        "scikit_learn",
-        "scipy",
-        "seaborn",
-        "statsmodels",
-        "taigapy",
-        "venn",
-        ],  # external packages as dependencies
+    install_requires=read_requirements("requirements.txt"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Intended Audience :: Science/Research",
@@ -73,7 +67,7 @@ print("You might want to install Bowtie2, samtools, bwa and R to be able to use 
 
 print("trying to install R packages")
 os.system(
-    "R -e \"if(!requireNamespace(\"BiocManager\", quietly = TRUE)){install.packages(\"BiocManager\", repos=\"http://cran.us.r-project.org\")};BiocManager::install(c(\"GSEABase\", \"erccdashboard\", \"GSVA\", \"DESeq2\"));\"")
+    'R -e "if(!requireNamespace(\"BiocManager\", quietly = TRUE)){install.packages(\"BiocManager\", repos=\"http://cran.us.r-project.org\")};BiocManager::install(c(\"GSEABase\", \"erccdashboard\", \"GSVA\", \"DESeq2\"));"')
 print('if it did not work. please install R or check your R installation')
 print("once R is installed you need to install erccdashboard, GSEABase GSVA, DESeq2 to have access to all the functions")
 
